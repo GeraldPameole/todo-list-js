c202657dc55dac26d055f7f3c94a4a0245bae33a
GeraldPameole <31417731+GeraldPameole@users.noreply.github.com>
2024-02-07 22:14:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -16,29 +16,28 @@ todoForm.addEventListener('submit', (event) => {
     alert('Inscrivez une tâche!');
     return;
   }
-  todoInput.value = '';
-});
-/* Dans cette fonction :
-=> Empêcher le comportement de soumission de formulaire par défaut, qui actualise la page.
-=> Vérifier si le champ de saisie est vide et alerter l'utilisateur si c'est le cas.
-=> Préparation de l'ajout de la nouvelle tâche (que nous traiterons dans les prochaines étapes).
-=> Effacement du champ de saisie après l'ajout d'une tâche.*/
 
-/* Création d'une fonction pour ajouter des tâches*/
-function addTask(task) {
+  /* Intégration de la fonction Ajouter une tâche */
+  todoForm.addEventListener('submit', function (event) {
 
-  const listItem = document.createElement('li');
-  listItem.textContent = task;
+    addTask(newTask);
+    todoInput.value = '';
+  });
+  /* Dans cette fonction :
+  => Empêcher le comportement de soumission de formulaire par défaut, qui actualise la page.
+  => Vérifier si le champ de saisie est vide et alerter l'utilisateur si c'est le cas.
+  => Préparation de l'ajout de la nouvelle tâche (que nous traiterons dans les prochaines étapes).
+  => Effacement du champ de saisie après l'ajout d'une tâche.*/
 
-  todoList.appendChild(listItem);
-}
-/*Dans cette fonction :
-=> Création d'un nouvel élément <li> pour la tâche.
-=> Définir son contenu textuel sur la tâche transmise à la fonction.
-=> Ajout du nouvel élément de liste à notre élément todo-list .*/
+  /* Création d'une fonction pour ajouter des tâches*/
+  function addTask(task) {
+    const listItem = document.createElement('li');
+    listItem.textContent = task;
 
-/*  Intégration de la fonction Ajouter une tâche */
-todoForm.addEventListener('submit', function (event) {
-  // Existing code
-  addTask(newTask); // Add the new task
+    todoList.appendChild(listItem);
+  }
+  /*Dans cette fonction :
+  => Création d'un nouvel élément <li> pour la tâche.
+  => Définir son contenu textuel sur la tâche transmise à la fonction.
+  => Ajout du nouvel élément de liste à notre élément todo-list .*/
 });
